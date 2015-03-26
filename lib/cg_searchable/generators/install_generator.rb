baa55a1e55651dedb43ad05ebe96b3af0dac6675
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2015-03-26 14:39:46 -0400

    Rename migration

--- lib/cg_searchable/generators/install_generator.rb
+++ lib/cg_searchable/generators/install_generator.rb
@@ -10,7 +10,7 @@ module CgSearchable
       end
 
       def copy_migrations
-        migration_template "migrations/create_pg_search_configurations.rb", "db/migrate/cg_searchable.rb"
+        migration_template "migrations/create_pg_search_configurations.rb", "db/migrate/create_pg_search_configurations.rb"
       end
 
       def self.next_migration_number(path)
