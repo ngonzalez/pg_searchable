ae4eb1b62432e1c835c3e3ca3f1fc1d326ced4e4
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2015-03-26 16:16:43 -0400

    Update Migration

--- lib/cg_searchable/generators/templates/migrations/create_pg_search_configurations.rb
+++ lib/cg_searchable/generators/templates/migrations/create_pg_search_configurations.rb
@@ -3,14 +3,11 @@ class CreatePgSearchConfigurations < ActiveRecord::Migration
 
     ActiveRecord::Base.connection.execute("CREATE EXTENSION IF NOT EXISTS unaccent;")
     I18n.available_locales.each do |locale|
-      stem = locale == :en ? 'english_stem' : locale == :fr ? 'french_stem' : 'english_stem'
       ActiveRecord::Base.connection.execute("DROP TEXT SEARCH CONFIGURATION IF EXISTS search_cfg_#{locale}")
       ActiveRecord::Base.connection.execute("CREATE TEXT SEARCH CONFIGURATION search_cfg_#{locale} (parser=default);")
       ActiveRecord::Base.connection.execute("ALTER TEXT SEARCH CONFIGURATION search_cfg_#{locale} ADD MAPPING FOR version WITH simple;")
+      stem = locale == :en ? 'english_stem' : locale == :fr ? 'french_stem' : 'english_stem'
       ActiveRecord::Base.connection.execute("ALTER TEXT SEARCH CONFIGURATION search_cfg_#{locale} ADD MAPPING FOR asciiword WITH #{stem};")
-      ActiveRecord::Base.connection.execute("DROP TEXT SEARCH DICTIONARY IF EXISTS syn_dictionary_#{locale}")
-      ActiveRecord::Base.connection.execute("CREATE TEXT SEARCH DICTIONARY syn_dictionary_#{locale} (TEMPLATE = synonym, SYNONYMS = synonym_sample);")
-      ActiveRecord::Base.connection.execute("ALTER TEXT SEARCH CONFIGURATION search_cfg_#{locale} ALTER MAPPING FOR asciiword WITH syn_dictionary_#{locale}, french_stem;")
     end
 
   end
