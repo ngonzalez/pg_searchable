28306b53b2662ecd2bc3d17aec65ffc626fe6d38
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2015-03-27 10:08:20 -0400

    Update migration

--- lib/cg_searchable/generators/templates/migrations/create_pg_search_configurations.rb
+++ lib/cg_searchable/generators/templates/migrations/create_pg_search_configurations.rb
@@ -2,11 +2,13 @@ class CreatePgSearchConfigurations < ActiveRecord::Migration
   def change
 
     ActiveRecord::Base.connection.execute("CREATE EXTENSION IF NOT EXISTS unaccent;")
-    I18n.available_locales.each do |locale|
-      ActiveRecord::Base.connection.execute("DROP TEXT SEARCH CONFIGURATION IF EXISTS search_cfg_#{locale}")
-      ActiveRecord::Base.connection.execute("CREATE TEXT SEARCH CONFIGURATION search_cfg_#{locale} (parser=default);")
-      ActiveRecord::Base.connection.execute("ALTER TEXT SEARCH CONFIGURATION search_cfg_#{locale} ADD MAPPING FOR version WITH simple;")
-      ActiveRecord::Base.connection.execute("ALTER TEXT SEARCH CONFIGURATION search_cfg_#{locale} ADD MAPPING FOR asciiword WITH #{locale == :fr ? 'french_stem' : 'english_stem'};")
+    [{ en: "english_stem"}, { fr: "french_stem" }].each do |hash|
+      hash.each do |locale, stemmer|
+        ActiveRecord::Base.connection.execute("DROP TEXT SEARCH CONFIGURATION IF EXISTS search_cfg_#{locale}")
+        ActiveRecord::Base.connection.execute("CREATE TEXT SEARCH CONFIGURATION search_cfg_#{locale} (parser=default);")
+        ActiveRecord::Base.connection.execute("ALTER TEXT SEARCH CONFIGURATION search_cfg_#{locale} ADD MAPPING FOR version WITH simple;")
+        ActiveRecord::Base.connection.execute("ALTER TEXT SEARCH CONFIGURATION search_cfg_#{locale} ADD MAPPING FOR asciiword WITH #{stemmer};")
+      end
     end
 
   end
