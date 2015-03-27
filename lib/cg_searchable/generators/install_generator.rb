d80792b3ca0177fed65f8368087e5d74dd320f95
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2015-03-26 22:44:44 -0400

    Fix NotImplementedError next_migration_number

--- lib/cg_searchable/generators/install_generator.rb
+++ lib/cg_searchable/generators/install_generator.rb
@@ -1,21 +1,16 @@
-include Rails::Generators::Migration
-
 module CgSearchable
   module Generators
-    class InstallGenerator < Rails::Generators::Base
+    class InstallGenerator < ::Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
+
+      include Rails::Generators::Migration
 
       def copy_migrations
         migration_template "migrations/create_pg_search_configurations.rb", "db/migrate/create_pg_search_configurations.rb"
       end
 
-      def self.next_migration_number(path)
-        unless @prev_migration_nr
-          @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
-        else
-          @prev_migration_nr += 1
-        end
-        @prev_migration_nr.to_s
+      def self.next_migration_number path
+        Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
       end
 
     end
